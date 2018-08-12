8a2088070f305423067d32b80177f48b1b907309
ABeens <41970794+ABeens@users.noreply.github.com>
2018-08-11 19:27:07 -0600

    Inclusion de procesos

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,28 @@
-$( document ).ready(function(){
+$(document).ready(function () {
     $(".banner-text").delay(500).animate({ opacity: 1 }, 700);
+
+    $(".products div").mouseenter(function () {
+        $(this).animate({ opacity: 0.7 }, 700);
+    })
+
+    $(".products div").mouseleave(function () {
+        $(this).animate({ opacity: 1 }, 700);
+    })
+    console.log(document.getElementById("mySidenav").style);
 })
+
+function openNav() {
+        if ((document.getElementById("mySidenav").style.width) && (document.getElementById("mySidenav").style.width != "0px")) {
+            document.getElementById("mySidenav").style.width = "0";
+        }
+        else {
+            document.getElementById("mySidenav").style.width = "100%";
+        }
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
